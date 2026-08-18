6bfea29c8a173c1d89d15e3679ca4a7ab6f5fdfd
Impendable <105776541+Impendable@users.noreply.github.com>
2026-08-18 12:58:31 -0400

    Partial implementation of growing cycle

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,11 +2,23 @@ extends Control
 
 @onready var pot_grid: GridContainer = $PotGrid
 
+signal next_day
+
+var current_day := 1
 var pot: Pot
 
 func _ready() -> void:
 	for pot in pot_grid.get_children():
 		pot.tapped.connect(_on_pot_tapped)
+	$HeaderUI/DayLabel.text = "Day: %s" % current_day
+
 
 func _on_pot_tapped(pot: Pot) -> void:
+	pot.interact()
 	print("%s tapped, state = %s" % [pot.name, Pot.State.keys()[pot.state]])
+
+
+func _on_next_day_pressed(pot: Pot) -> void:
+	current_day += 1
+	next_day.emit()
+	
--- scenes/pot.gd
+++ scenes/pot.gd
@@ -1,12 +1,39 @@
 class_name Pot
 extends Button
 
+@onready var main_path: NodePath = ^"/root/scenes/main"
+
 signal tapped(pot: Pot)
 
 enum State { EMPTY, GROWING, READY }
+const GROWTH_DAYS := 3
+const SELL_PRICE := 10
 
 var state: State = State.EMPTY
+var growth_counter: int
 
+func _ready() -> void:
+	main_path.next_day.connect(advance_day)
 
 func _on_pressed() -> void:
 	tapped.emit(self)
+
+func advance_day():
+	growth_counter += 1
+	if growth_counter == GROWTH_DAYS:
+		growth_counter = 0
+		state = State.READY
+
+func interact():
+	match state:
+		"EMPTY":
+			print("Planting...")
+			state = State.GROWING
+		"READY":
+			print("Harvesting...")
+			state = State.EMPTY
+		"GROWING":
+			print("Plant is growing!")
+	
+func _refresh():
+	self.text = State.keys()[state]
