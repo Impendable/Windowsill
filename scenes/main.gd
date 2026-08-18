8ac77a99ca68198308fa764f88f914daace586bc
Impendable <105776541+Impendable@users.noreply.github.com>
2026-08-18 15:52:01 -0400

    Attempting to get action budget implemented

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,18 +1,33 @@
 extends Control
 
-@onready var pot_grid: GridContainer = $PotGrid
+@onready var pot_grid: HBoxContainer = $PotGrid
 
 var current_day := 1
 var current_coins := 0
+var remaining_actions := 5
 
 func _ready() -> void:
 	for pot: Pot in pot_grid.get_children():
 		pot.tapped.connect(_on_pot_tapped)
-
+	_refresh_header()
 
 
 func _on_pot_tapped(pot: Pot) -> void:
-	current_coins += pot.interact()
+	if remaining_actions > 0:
+		var result := pot.interact()
+		if result == 1:
+			remaining_actions -= 1
+			print("PLANT ACTION")
+		elif result  == 2:
+			remaining_actions -= 1
+			print("HARVEST ACTION")
+			pot.harvested.connect(_sell_plant)
+		else:
+			return
+	else:
+		print("No actions remaining")
+		return
+		
 	print("%s tapped, state = %s" % [pot.name, Pot.State.keys()[pot.state]])
 	_refresh_header()
 
@@ -21,9 +36,15 @@ func _on_next_day_pressed() -> void:
 	current_day += 1
 	for pot: Pot in pot_grid.get_children():
 		pot.advance_day()
+	remaining_actions = 5
 	_refresh_header()
 	
 func _refresh_header() -> void:
 	$HeaderUI/CoinLabel.text = "Coins: %s" % current_coins
 	$HeaderUI/DayLabel.text = "Day: %s" % current_day
+	$HeaderUI/ActionBudget.text = "Actions: %s" % remaining_actions
 	
+	
+func _sell_plant(amount: int) -> void:
+	current_coins += amount
+	_refresh_header()
--- scenes/pot.gd
+++ scenes/pot.gd
@@ -1,44 +1,46 @@
 class_name Pot
 extends Button
 
-@onready var main_path: NodePath = ^"/root/scenes/main"
-
 signal tapped(pot: Pot)
+signal harvested(pot: Pot)
 
 enum State { EMPTY, GROWING, READY }
+enum Result { NONE, PLANTED, HARVESTED }
 const GROWTH_DAYS := 3
 const SELL_PRICE := 10
 
 var state: State = State.EMPTY
+var result: Result = Result.NONE
+
 var growth_counter: int
 
 func _on_pressed() -> void:
 	tapped.emit(self)
 
-func advance_day():
+func advance_day() -> void:
 	if state == State.GROWING:
 		growth_counter += 1
-	if growth_counter >= GROWTH_DAYS:
-		growth_counter = 0
-		state = State.READY
+		if growth_counter >= GROWTH_DAYS:
+			growth_counter = 0
+			state = State.READY
 	_refresh()
 
-func interact():
+func interact() -> int:
 	match state:
 		State.EMPTY:
 			print("Planting...")
 			state = State.GROWING
-			_refresh()
-			return 0
+			result = Result.PLANTED
 		State.READY:
 			print("Harvesting...")
+			harvested.emit(self, SELL_PRICE)
 			state = State.EMPTY
-			_refresh()
-			return SELL_PRICE
-		State.EMPTY:
+			result = Result.HARVESTED
+		State.GROWING:
 			print("Plant is growing!")
-			_refresh()
-			return 0
-	
-func _refresh():
+			result = Result.NONE
+	_refresh()
+	return result
+
+func _refresh() -> void:
 	self.text = State.keys()[state]
